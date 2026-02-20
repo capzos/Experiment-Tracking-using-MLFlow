8f24557844dc86a7612e8b3cf40d7d0172251043
Raj <rajkp1302@gmail.com>
2026-02-20 23:23:16 +0530

    experiment1 tracked using mlflow

--- mlartifacts/1/905b7cd7799a4dca987fbaa0cee8afae/artifacts/file1.py
+++ mlartifacts/1/905b7cd7799a4dca987fbaa0cee8afae/artifacts/file1.py
@@ -7,25 +7,23 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import dagshub
-dagshub.init(repo_owner='vikashdasXXX', repo_name='YT-MLOPS-Experiments-with-MLFlow', mlflow=True)
 
-mlflow.set_tracking_uri("https://dagshub.com/vikashdas770/")
+# Set the tracking URI to your MLflow server
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
-# Load Wine dataset
+# Load Wine dataset and split into train and test sets
 wine = load_wine()
 X = wine.data
 y = wine.target
 
-# Train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
 
 # Define the params for RF model
-max_depth = 8
+max_depth = 10
 n_estimators = 5
 
 # Mention your experiment below
-mlflow.set_experiment('YT-MLOPS-Exp2')
+mlflow.set_experiment('YT-MLOPS-Exp1-Random-Forest-Classifier')
 
 with mlflow.start_run():
     rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
@@ -54,7 +52,7 @@ with mlflow.start_run():
     mlflow.log_artifact(__file__)
 
     # tags
-    mlflow.set_tags({"Author": 'Vikash', "Project": "Wine Classification"})
+    mlflow.set_tags({"Author": 'Raj', "Project": "Wine Classification"})
 
     # Log the model
     mlflow.sklearn.log_model(rf, "Random-Forest-Model")
